f35c8fcbc4ef8f3a815d7485946f6cab773fdf31
kiranrouthuRS <61677679+kiranrouthuRS@users.noreply.github.com>
2020-08-06 20:41:21 +0530

    Dynamic questions for client page

--- src/AppShell.tsx
+++ src/AppShell.tsx
@@ -1,30 +1,31 @@
 /** @jsx jsx */
 import React from "react";
 import { jsx, css } from "@emotion/core";
+import { connect } from "react-redux";
 import { withRouter, Route } from "react-router-dom";
 import { Link as RouterLink, useHistory } from "react-router-dom";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-import {connect} from "react-redux";
 import * as user from "./redux-modules/user";
 import { AppState } from "./redux-modules/root";
 import {  Global } from "@emotion/core";
 import { domainPath } from "./App"
-
+import { loginApiUrl } from "./api/api"
+import { store } from "./index"; 
 import {
   ConfigIcon,
   NewClientIcon,
   ExistingClientIcon
 } from "./components/icons";
-
+ 
 const App = css`
   margin: 80px auto;
   width: 100%;
   background-color: #fff;
   padding: 16px;
   position: relative;
-  @media all and (min-width: 520px) {
+  @media all and (min-width: 520px) { 
     padding: 40px;
     margin: 100px auto;
     width: 60vw;
@@ -100,7 +101,7 @@ const firstMatchLogo = css`
 const adelphoiLogo = css`
   position: absolute;
   top: -25px;
-  left: -25px;
+  left: -12px;
   @media all and (max-width: 520px) {
     top: 0;
     right: 0;
@@ -123,13 +124,26 @@ const profile = css`
   radius: 2px;
   @media all and (max-width: 520px) {
     top: 0;
-    right: 0;
+    right: 25;
+  }
+`;
+
+const billing = css`
+  position: absolute;
+  top: -25px;
+  right: 175px;
+  radius: 2px;
+  @media all and (max-width: 520px) {
+    top: 0;
+    right: 15;
   }
 `;
 
 const AppShell: React.FC = ({children},props ) => {
- console.log(props,"shell")
-  return (
+//  const is_configured = children&&children[1].props.user.user.is_fully_configured  
+//   const logopath = children&&children[1].props.user.user.logo_path; 
+  
+ return ( 
     <Paper css={App} elevation={3}>
       
       <div css={logo}>
@@ -141,8 +155,14 @@ const AppShell: React.FC = ({children},props ) => {
         <img
           css={adelphoiLogo}
           alt={`${domainPath} Logo`}
-          src={`/img/${domainPath}_logo.png`}
+          src={`${loginApiUrl}/${logopath}`}
         />
+        <a
+              href={`/${domainPath}/billing`}
+              css={billing}
+            >
+              Billing
+            </a>
         <a
               href={`/${domainPath}/changepassword`}
               css={profile}
@@ -156,18 +176,17 @@ const AppShell: React.FC = ({children},props ) => {
               Logout
             </a>
       </div>
-      {/* {is_pwd_updated === true ?  : ""} */}
-      { domainPath == "adelphoi" ? (
+      
+      { is_configured == true ? (
                  <div css={nav}>
                  <Route
-                   path={`/${domainPath}/new-client`}
+                   path={domainPath === "adelphoi"?`/${domainPath}/new-client`:`/${domainPath}/new-client1`}
                    // exact={activeOnlyWhenExact}
-                   children={({ match, history }) => (
+                   children={({ match, history }) => ( 
                      <Link
                        onClick={() => {
-                         history.push(`/${domainPath}/new-client`);
+                         history.push(domainPath === "adelphoi"?`/${domainPath}/new-client`:`/${domainPath}/new-client1`);
                        }}
-                       // href="#"
                        css={menuButton}
                        style={
                          match
@@ -186,7 +205,6 @@ const AppShell: React.FC = ({children},props ) => {
          
                  <Route
                    path={`/${domainPath}/existing-client`}
-                   // exact={activeOnlyWhenExact}
                    children={({ match, history }) => (
                      <Link
                        onClick={() => {
@@ -305,12 +323,19 @@ const AppShell: React.FC = ({children},props ) => {
       </div>)}  
       
       {children}
+      
     </Paper>
     
   );
 };
 
+const mapStateToProps = (state: AppState) => {
+  return {
+    user: state.user,
+    appState: state,
+  }
+};
 
+export default connect(mapStateToProps)(withRouter(AppShell));
 
-export default withRouter(AppShell);
  
