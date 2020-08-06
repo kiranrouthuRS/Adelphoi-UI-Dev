17e5226ea77e75f6b4765cce7662ab696935426c
kiranrouthuRS <61677679+kiranrouthuRS@users.noreply.github.com>
2020-08-06 21:20:04 +0530

    Dynamic questions for client page

--- src/AppShell.tsx
+++ src/AppShell.tsx
@@ -6,25 +6,25 @@ import { Link as RouterLink, useHistory } from "react-router-dom";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-import {connect} from "react-redux";
 import * as user from "./redux-modules/user";
 import { AppState } from "./redux-modules/root";
 import {  Global } from "@emotion/core";
 import { domainPath } from "./App"
-
+import { loginApiUrl } from "./api/api"
+import { store } from "./index"; 
 import {
   ConfigIcon,
   NewClientIcon,
   ExistingClientIcon
 } from "./components/icons";
-
+ 
 const App = css`
   margin: 80px auto;
   width: 100%;
   background-color: #fff;
   padding: 16px;
   position: relative;
-  @media all and (min-width: 520px) {
+  @media all and (min-width: 520px) { 
     padding: 40px;
     margin: 100px auto;
     width: 60vw;
@@ -100,7 +100,7 @@ const firstMatchLogo = css`
 const adelphoiLogo = css`
   position: absolute;
   top: -25px;
-  left: -25px;
+  left: -12px;
   @media all and (max-width: 520px) {
     top: 0;
     right: 0;
@@ -123,13 +123,28 @@ const profile = css`
   radius: 2px;
   @media all and (max-width: 520px) {
     top: 0;
-    right: 0;
+    right: 25;
+  }
+`;
+
+const billing = css`
+  position: absolute;
+  top: -25px;
+  right: 175px;
+  radius: 2px;
+  @media all and (max-width: 520px) {
+    top: 0;
+    right: 15;
   }
 `;
 
 const AppShell: React.FC = ({children},props ) => {
- console.log(props,"shell")
-  return (
+// const is_configured = children&&children[1].props.user.user.is_fully_configured  
+// const logopath = children&&children[1].props.user.user.logo_path; 
+const is_configured = store.getState().user.user.is_fully_configured  
+const logopath = store.getState().user.user.logo_path;  
+  
+ return ( 
     <Paper css={App} elevation={3}>
       
       <div css={logo}>
@@ -141,8 +156,14 @@ const AppShell: React.FC = ({children},props ) => {
         <img
           css={adelphoiLogo}
           alt={`${domainPath} Logo`}
-          src={`/img/${domainPath}_logo.png`}
+          src={`${loginApiUrl}/${logopath}`}
         />
+        <a
+              href={`/${domainPath}/billing`}
+              css={billing}
+            >
+              Billing
+            </a>
         <a
               href={`/${domainPath}/changepassword`}
               css={profile}
@@ -156,18 +177,16 @@ const AppShell: React.FC = ({children},props ) => {
               Logout
             </a>
       </div>
-      {/* {is_pwd_updated === true ?  : ""} */}
-      { domainPath == "adelphoi" ? (
-                 <div css={nav}>
+      
+           <div css={nav}>
                  <Route
-                   path={`/${domainPath}/new-client`}
+                   path={domainPath === "adelphoi"?`/${domainPath}/new-client`:`/${domainPath}/new-client1`}
                    // exact={activeOnlyWhenExact}
-                   children={({ match, history }) => (
+                   children={({ match, history }) => ( 
                      <Link
                        onClick={() => {
-                         history.push(`/${domainPath}/new-client`);
+                         history.push(domainPath === "adelphoi"?`/${domainPath}/new-client`:`/${domainPath}/new-client1`);
                        }}
-                       // href="#"
                        css={menuButton}
                        style={
                          match
@@ -175,18 +194,18 @@ const AppShell: React.FC = ({children},props ) => {
                            : { backgroundColor: "#f5f5f5", color: "#9d9d9d" }
                        }
                      >
-                       <NewClientIcon
+                      {is_configured === true ? <NewClientIcon
                          css={menuIcon}
                          fillColor={match ? "white" : "#9d9d9d"}
                        />
-                       New Client
+                       :""}
+                      {is_configured === true ? "New Client" :""}
                      </Link>
                    )}
                  />
          
                  <Route
                    path={`/${domainPath}/existing-client`}
-                   // exact={activeOnlyWhenExact}
                    children={({ match, history }) => (
                      <Link
                        onClick={() => {
@@ -199,11 +218,11 @@ const AppShell: React.FC = ({children},props ) => {
                            : { backgroundColor: "#f5f5f5", color: "#9d9d9d" }
                        }
                      >
-                       <ExistingClientIcon
+                      {is_configured === true ? <ExistingClientIcon
                          css={menuIcon}
                          fillColor={match ? "white" : "#9d9d9d"}
-                       />
-                       Existing Client 
+                       />:""}
+                     {is_configured === true ?  "Existing Client" :""} 
                      </Link>
                    )}
                  />
@@ -231,9 +250,9 @@ const AppShell: React.FC = ({children},props ) => {
                    )}
                  />
                </div>
-      ) : (
+     
         <div css={nav}>
-      <Route
+      {/* <Route
         path={`/${domainPath}/configuration`}
         // exact={activeOnlyWhenExact}
         children={({ match, history }) => (
@@ -248,69 +267,20 @@ const AppShell: React.FC = ({children},props ) => {
                 : { backgroundColor: "#f5f5f5", color: "#9d9d9d" }
             }
           >
-            {/* <ConfigIcon
-              css={menuIcon}
-              fillColor={match ? "white" : "#9d9d9d"}
-            />
-            Configuration */}
+           
           </Link>
         )}
-      />
-      <Route
-        path={`/${domainPath}/configuration`}
-        // exact={activeOnlyWhenExact}
-        children={({ match, history }) => (
-          <Link
-            onClick={() => {
-              history.push(`/${domainPath}/configuration/users`);
-            }}
-            css={menuButton}
-            style={
-              match
-                ? { backgroundColor: "#8284e5", color: "white" }
-                : { backgroundColor: "#f5f5f5", color: "#9d9d9d" }
-            }
-          >
-            <ConfigIcon
-              css={menuIcon}
-              fillColor={match ? "white" : "#9d9d9d"}
-            />
-            Configuration
-          </Link>
-        )}
-      />
-      <Route
-        path={`/${domainPath}/configuration`}
-        // exact={activeOnlyWhenExact}
-        children={({ match, history }) => (
-          <Link
-            onClick={() => {
-              history.push(`/${domainPath}/configuration/users`);
-            }}
-            css={menuButton}
-            style={
-              match
-                ? { backgroundColor: "#8284e5", color: "white" }
-                : { backgroundColor: "#f5f5f5", color: "#9d9d9d" }
-            }
-          >
-            {/* <ConfigIcon
-              css={menuIcon}
-              fillColor={match ? "white" : "#9d9d9d"}
-            />
-            Configuration */}
-          </Link>
-        )}
-      />
-      </div>)}  
+      /> */}
+      
+      </div>
       
       {children}
+      
     </Paper>
     
   );
 };
 
-
-
 export default withRouter(AppShell);
+
  
