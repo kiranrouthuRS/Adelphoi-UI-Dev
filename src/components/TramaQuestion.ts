a6af3a2d49da5757dfb26694392f34391155aa78
kiranrouthuRS <61677679+kiranrouthuRS@users.noreply.github.com>
2021-09-03 15:51:03 +0530

    client page issue

--- src/components/TramaQuestion.ts
+++ src/components/TramaQuestion.ts
@@ -7,59 +7,73 @@ export const Persues_House_Score =
 [
   {
  Question: "Number of Prior Treatment Placements",
- values : [0,1,1,1]
+ values : [0,1,1,1],
+ addValues: true
   },
    {
     Question: "Number of Prior RTF Placements",
-    values : [0,1,1,1]
+    values : [0,1,1,1],
+    addValues: true
    },
    {
     Question: "Number of Prior Foster Homes",
-    values : [0,1,1,1]
+    values : [0,1,1,1],
+    addValues: true
    },
    {
     Question: "Total Prior Unsuccessful Treatment Placements",
-    values : [0,1,1,1]
+    values : [0,1,1,1],
+    addValues: true
    },
    {
     Question: "Suicidality",
-    values : [0,1,1,1,1]
+    values : [0,1,1,1,1],
+    addValues: false
    },
    {
     Question: "Number of Previous Inpatient Hospitalizations",
-    values : [0,1,1,1]
+    values : [0,1,1,1],
+    addValues: true
    },
    {
     Question: "History of Abuse",
-    values : [1,0]
+    values : [1,0],
+    addValues: true
    },
    {
     Question: "History of Neglect",
-    values : [1,0,0]
+    values : [1,0,0],
+    addValues: true
    },
    {
     Question: "Family Involvement",
-    values : [0,1,1,1]
+    values : [0,1,1,1],
+    addValues: true
    },
    {
     Question: "Adopted",
-    values : [1,0]
+    values : [1,0],
+    addValues: true
    },
    {
     Question: "Deceased Cargegiver",
-    values : [1,0]
+    values : [1,0],
+    addValues: true
    },
    {
     Question: "Family Suicide History",
-    values : [0,1,1,1,1]
+    values : [0,1,1,1,1],
+    addValues: false
    },
    {
     Question: "Incarcerated Caregiver",
-    values : [1,0,0]
+    values : [1,0,0],
+    addValues: true
    },
    {
     Question: "Additional Traumatic Experiences",
-    values : [0,0,0,1]
+    values : [0,0,0,1],
+    addValues: true
    }
    ]
    : []
